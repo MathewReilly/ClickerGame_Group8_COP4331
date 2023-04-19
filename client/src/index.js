406146eaf872bfabf87cfd13f4008355d6e10d00
JSCaracal <siposj@knights.ucf.edu>
2023-04-18 22:16:10 -0400

    adds the initial files for me

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import HomePage from './HomePage';
 import GamePage from './GamePage';
 import reportWebVitals from './reportWebVitals';
-// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,6 +15,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
