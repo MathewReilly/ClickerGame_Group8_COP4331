4374b4dcb01cca42aea4c990c8366bfa48019bf2
Paul <08.goatee.fishy@icloud.com>
2023-04-18 22:47:39 -0400

    Getting register and login apis working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port = process.env.PORT || 5001;
+const cors = require('cors');
+const User = require('./models/user.model');
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5001;
 
 mongoose.connect('mongodb+srv://Paul:444gEdWyGidazCnZ@cluster0.e0trkiw.mongodb.net/clicker?retryWrites=true&w=majority');
 
@@ -14,9 +17,33 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
-// create a GET route
-app.get('/express_backend', (req, res) => { 
-  res.send({ express: 'This came from server.js!!!!' });
+app.post('/api/register', async (req, res) => {
+  console.log(req.body);
+  try {
+      await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+          nickname: req.body.nickname,
+      })
+      res.json({status: 'ok'})
+  } catch (err) {
+     res.json({status: 'error', error: 'Duplicate email' });
+  }
 });
 
-//test
+app.post('/api/login', async (req, res) => {
+      const user = await User.findOne({
+          email: req.body.email,
+          password: req.body.password,
+      })
+
+      if (user) {
+          return res.json({status: 'ok', user: true});
+      } else {
+          return res.json({status: 'error', user: false});
+       }
+});
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`));
