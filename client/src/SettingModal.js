12680c88c811235d5c5856bac433cbe9fc05de0a
Ross Springstead <ross.springstead@knights.ucf.edu>
2023-04-21 12:39:27 -0400

    change setting to about modal

--- client/src/SettingModal.js
+++ client/src/SettingModal.js
@@ -15,9 +15,17 @@ function SettingsModal({closeModal}){
               </button>
             </div>
             <div className="title">
-              <h1>Settings</h1>
+              <h1>About</h1>
+              <h2>Group 8 : Season Clicker</h2>
+              <h3>Welcome to our Large Project</h3>
+              <h4> How to Play! </h4>
+              <p>Click the icon in the center of the screen to increase your score by 8!</p> 
+              <p> You can save your score by pressing the Save button or by pressing the Logout button. </p> 
+              <p> Keep pressing to experience the changing of seasons and more suprises! </p>
             </div>
             <div className="body">
+            
+            
                 
             </div>
           </div>
